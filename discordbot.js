45fb217146663d22b44520deb7f7e91e879dd7ae
sjd4322 <sjd4322@gmail.com>
2020-01-23 18:20:39 -0500

    Testing out the webhooks functionality

--- discordbot.js
+++ discordbot.js
@@ -1,10 +1,20 @@
-const auth = require('./auth.json');
 var http = require('http');
 
 require('dotenv').config();
+const serverRequest = require('request');
 const port = 3000;
 
+//All the bs for jQuery to work on node.js
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+var $ = jQuery = require('jquery')(window);
+
 http.createServer(function (request, response) {
+    const auth = require('./auth.json');
+
     //Discord connect
     const Discord = require('discord.js');
     const client = new Discord.Client();
@@ -15,20 +25,58 @@ http.createServer(function (request, response) {
     const uri = require('./mongoConnection.json');
     const mongoClient = new MongoClient(uri.connectionString, { useNewUrlParser: true });
     const connection = mongoClient.connect();
+    
+    var express = require('express'); // Express web server framework
+    var app = express();
 
-    client.on("presenceUpdate", (oldUser, newUser) => {
-        //Exclude title changes and typing
-        if(oldUser.presence.game != null && oldUser.presence.game.streaming 
-            && newUser.presence.clientStatus !== "desktop"
-            && oldUser.presence.clientStatus !== "desktop"
-            || oldUser.guild.name !== "Clavaats Server") return;
+    app.use(express.urlencoded({ useUnifiedTopology: true }));
+
+    client.on("ready", () => {
+        $.ajax({
+            url: 'https://api.twitch.tv/helix/users?login=Clavaat',
+            type: "GET",
+            headers: {
+                'Client-ID': "o87hugnzwpgqe275s3g83hptpmkea2"
+            },
+            success: function(baseStream) {
+                var options = {
+                    url: 'https://api.twitch.tv/helix/webhooks/hub',
+                    method: 'POST',
+                    headers: {
+                        'Client-ID': "o87hugnzwpgqe275s3g83hptpmkea2",
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        'hub.mode':'subscribe',
+                        'hub.topic':'https://api.twitch.tv/helix/streams?user_id=' + baseStream.data[0].id,
+                        'hub.callback':'http://odinpi:3000/streamChanged',
+                        'hub.lease_seconds':'864000'
+                    })
+                };
         
-        if(oldUser.displayName === "Clavaat" && newUser.presence != null){
-            if(newUser.presence.game != null && newUser.presence.game.streaming){
-                client.channels.get("617196134536052737").send("@everyone Clavaat has started streaming! https://www.twitch.tv/clavaat");               
-            }
-        }
+                serverRequest.post(options);     
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                console.log("Status: " + textStatus); 
+                console.log("Error: " + errorThrown); 
+            }    
+        });
+
     });
+
+    // client.on("presenceUpdate", (oldUser, newUser) => {
+    //     //Exclude title changes and typing
+    //     if(oldUser.presence.game != null && oldUser.presence.game.streaming 
+    //         && newUser.presence.clientStatus !== "desktop"
+    //         && oldUser.presence.clientStatus !== "desktop"
+    //         || oldUser.guild.name !== "Clavaats Server") return;
+        
+    //     if(oldUser.displayName === "Clavaat" && newUser.presence != null){
+    //         if(newUser.presence.game != null && newUser.presence.game.streaming){
+    //             client.channels.get("617196134536052737").send("test");               
+    //         }
+    //     }
+    // });
 
     client.on("message", (message) => {
         if(message.channel.name === "recommendations"){
@@ -59,5 +107,14 @@ http.createServer(function (request, response) {
         }
     });
 
-    client.login(process.env.token);    
+    app.get('/streamChanged', function (req, res) {
+        res.send(req.query['hub.challenge']);             
+    });
+
+    app.post('/streamChanged', function(req, res){
+        res.sendStatus(202);
+        client.channels.get("617196134536052737").send("test");     
+    });
+
+    client.login(auth.token);    
 }).listen(port);
